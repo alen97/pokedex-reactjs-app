f6aaa66cb93c56beeedf7c8c47a1189c5477067e
Gabriel Alén Gauna <gabrielgna97@gmail.com>
2020-07-31 20:10:29 -0300

    Armado el esqueleto del proyecto y prototipo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
 function App() {
+
+  const pokemones = [
+    {
+      nombre: 'POKEMON 1'
+    },
+    {
+      nombre: 'POKEMON 2'
+    },
+    {
+      nombre: 'POKEMON 3'
+    },
+    {
+      nombre: 'POKEMON 4'
+    },
+    {
+      nombre: 'POKEMON 5'
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <React.Fragment>
+      <Grid container justify="center" alignItems="center" style={{ marginTop: 50, marginBottom: 50 }}>
+        <Grid item xs={(window.innerWidth >= 1366) ? 4 : (window.innerWidth < 1366 && window.innerWidth > 800) ? 6 : 10}>
+
+          <Grid container justify="center">
+            <Grid item xs>
+              <AppBar position="static">
+                <Toolbar style={{ alignSelf: "center" }}>
+                  <Typography>
+                    POKEDEX
+                  </Typography>
+                </Toolbar>
+              </AppBar>
+            </Grid>
+          </Grid>
+
+          <Grid container justify="center">
+            <Grid item xs style={{ backgroundColor: "gray" }}>
+           
+              {pokemones.map((pokemon) => (
+                <p style={{ textAlign: "center" }}>
+                  <ButtonBase style={{backgroundColor: "white", height:"150px", width: "150px"}}>{pokemon.nombre}</ButtonBase>
+                </p>
+              ))}
+
+            </Grid>
+            
+          </Grid>
+
+        </Grid>
+      </Grid>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <BrowserRouter>
+      {/* <Redirect
+        from="/"
+        to="/home" /> */}
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={App} />
+        <Route
+          exact
+          path="/pokemon/1"
+          render={() =>
+
+            <h1>POKEMON 1</h1>
+
+          } />
+
+        <Route render={() =>
+          <div>
+            <h1 style={{ color: "black", textAlign: "center" }}>404 – Page Not Found.</h1>
+            <h2 style={{ color: "black", textAlign: "center" }}>No pudimos encontrar la página que estás buscando.</h2>
+          </div>
+        } />
+      </Switch>
+
+    </BrowserRouter>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
