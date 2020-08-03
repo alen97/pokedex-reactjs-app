dbcca23153dc5a96a0d3327b897f4ffd54d04c09
Gabriel Alén Gauna <gabrielgna97@gmail.com>
2020-08-03 17:13:27 -0300

    Update .gitignore

---
+++ build/precache-manifest.d65afa47dc2c28a7d488fc1e556f14bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5eaecab9c7c44f198b56f9b4cf52f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b103bedb60899f92cba",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c22cf8d302bf1d5f7883",
+    "url": "/static/js/2.6880949d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6880949d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b103bedb60899f92cba",
+    "url": "/static/js/main.dceced12.chunk.js"
+  },
+  {
+    "revision": "8b53897bc9935d65a7b5",
+    "url": "/static/js/runtime-main.20a0f47d.js"
+  }
+]);
